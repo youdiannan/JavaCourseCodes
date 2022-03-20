a2c76f8d3fa675322f8505e3cea02e4daae1b0f7
yangjun <yang8863j@163.com>
2022-03-20 18:00:38 +0800

    week03: doesn't work

--- 02nio/nio02/src/main/java/io/github/kimmking/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
+++ 02nio/nio02/src/main/java/io/github/kimmking/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
@@ -1,22 +1,36 @@
 package io.github.kimmking.gateway.outbound.netty4;
 
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
 
-public class NettyHttpClientOutboundHandler  extends ChannelInboundHandlerAdapter {
-    
-    @Override
-    public void channelActive(ChannelHandlerContext ctx)
-            throws Exception {
-        
-        
+public class NettyHttpClientOutboundHandler extends SimpleChannelInboundHandler<FullHttpResponse> {
+
+    private ChannelHandlerContext inboundCtx;
+
+    public NettyHttpClientOutboundHandler(ChannelHandlerContext ctx) {
+        this.inboundCtx = ctx;
     }
-    
+
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-            throws Exception {
-        
-        
-        
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpResponse response) throws Exception {
+        if (response != null) {
+            if (!HttpUtil.isKeepAlive(response)) {
+                inboundCtx.write(response).addListener(ChannelFutureListener.CLOSE);
+            } else {
+                //response.headers().set(CONNECTION, KEEP_ALIVE);
+                inboundCtx.write(response);
+            }
+        }
+        inboundCtx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("forward error!");
+        cause.printStackTrace();
+        super.exceptionCaught(ctx, cause);
     }
 }

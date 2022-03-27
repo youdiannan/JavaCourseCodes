b65863cf36972811053eca6cfec86df922c6cf23
yangjun <yang8863j@163.com>
2022-03-27 19:13:15 +0800

    第4周作业

---
+++ 03concurrency/0301/README.md
@@ -0,0 +1,9 @@
+# 第4周作业
+
+## 第2道题
+
+见 HomeWork03.java
+
+## 第6道题
+
+见 Java并发编程脑图.png | Java并发编程脑图.xmind
--- 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
+++ 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
@@ -1,6 +1,8 @@
 package java0.conc0303;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
@@ -11,20 +13,88 @@ import java.util.concurrent.CountDownLatch;
  */
 public class Homework03 {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         
         long start=System.currentTimeMillis();
 
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
-    
-        int result = sum(); //这是得到的返回值
-        
-        // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
-         
+
+        // 1. Future
+//        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+//        Future<Integer> result = threadPool.submit(() -> sum());
+//        // 确保  拿到result 并输出
+//        System.out.println("异步计算结果为："+result.get());
+//        threadPool.shutdown();
+//
+//        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 2. FutureTask
+//        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+//        FutureTask<Integer> futureTask = new FutureTask<>(() -> sum());
+//        threadPool.execute(futureTask);
+//        System.out.println("异步计算结果为："+ futureTask.get());
+//        threadPool.shutdown();
+//
+//        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 3. CompletableFuture
+//        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> sum());
+//        System.out.println("异步计算结果为："+ completableFuture.join());
+//        System.out.println("异步计算结果为："+ completableFuture.get());
+//        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // or
+//        CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> sum()).thenAccept(result -> {
+//            System.out.println("异步计算结果为："+ result);
+//            System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+//        });
+//        completableFuture.join();
+
+
+        // 4. AtomicInteger
+//        AtomicInteger result = new AtomicInteger();
+//        Thread t = new Thread(() -> result.set(sum()));
+//        t.start();
+//
+//        for (;;) {
+//            if (result.get() != null) break;
+//        }
+//
+//        System.out.println("异步计算结果为："+ result);
+//        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 5. CountDownLatch
+//        CountDownLatch latch = new CountDownLatch(1);
+//        AtomicInteger result = new AtomicInteger();
+//        Thread t = new Thread(() -> {
+//            result.set(sum());
+//            latch.countDown();
+//        });
+//        t.start();
+//
+//        latch.await();
+//        // or 与t.wait()相同
+//        t.join();
+//
+//        System.out.println("异步计算结果为："+ result.get());
+//        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 6. park & unpark
+
+        Thread currentThread = Thread.currentThread();
+        AtomicInteger result = new AtomicInteger();
+        Thread t = new Thread(() -> {
+            result.set(sum());
+            LockSupport.unpark(currentThread);
+        });
+        t.start();
+
+        LockSupport.park();
+
+        System.out.println("异步计算结果为："+ result.get());
         System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
-        
+
         // 然后退出main线程
     }
     

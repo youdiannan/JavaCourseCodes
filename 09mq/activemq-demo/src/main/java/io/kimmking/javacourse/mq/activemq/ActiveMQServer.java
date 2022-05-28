46570df68d4f79de759605a31017818eec44a22d
yangjun <yang8863j@163.com>
2022-05-28 17:06:17 +0800

    搭建 ActiveMQ 服务，基于 JMS，写代码分别实现对于 queue 和 topic 的消息生产和消费，代码提交到 github。

--- 09mq/activemq-demo/src/main/java/io/kimmking/javacourse/mq/activemq/ActiveMQServer.java
+++ 09mq/activemq-demo/src/main/java/io/kimmking/javacourse/mq/activemq/ActiveMQServer.java
@@ -1,9 +1,18 @@
 package io.kimmking.javacourse.mq.activemq;
 
+import org.apache.activemq.broker.BrokerService;
+
 public class ActiveMQServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 尝试用java代码启动一个ActiveMQ broker server
         // 然后用前面的测试demo代码，连接这个嵌入式的server
+        BrokerService brokerService = new BrokerService();
+        brokerService.addConnector("tcp://localhost:61616");
+        brokerService.start();
+
+        while (true) {
+            Thread.sleep(60000);
+        }
     }
 }

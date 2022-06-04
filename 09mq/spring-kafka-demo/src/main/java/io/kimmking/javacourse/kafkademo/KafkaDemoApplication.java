3fed5da043e2b7b75ad5fb5fdb53d929dfbc8b11
yangjun <yang8863j@163.com>
2022-06-04 23:05:58 +0800

    1.（必做）搭建一个 3 节点 Kafka 集群，测试功能和性能；实现 spring kafka 下对 kafka 集群的操作，将代码提交到 github。

--- 09mq/spring-kafka-demo/src/main/java/io/kimmking/javacourse/kafkademo/KafkaDemoApplication.java
+++ 09mq/spring-kafka-demo/src/main/java/io/kimmking/javacourse/kafkademo/KafkaDemoApplication.java
@@ -1,13 +1,49 @@
 package io.kimmking.javacourse.kafkademo;
 
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.Scanner;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(KafkaDemoApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            AtomicInteger count = new AtomicInteger(0);
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext()) {
+                    String message = "generated message = "
+                            + UUID.randomUUID() + ", id=" + count.getAndIncrement();
+                    kafkaTemplate.send("spring-kafka-test",  message);
+                    System.out.println("message send: " + message);
+                    scanner.next();
+                }
+            }
+        };
+    }
+
+    @KafkaListener(topics = "spring-kafka-test")
+    public void consume(String message) {
+        System.out.println("message received: " + message);
     }
 
 }
